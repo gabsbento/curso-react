a03f686d228bc2842ca4c30b00f56ddbd5bdd2f3
Gabriel Bento <gab.silva.bento@gmail.com>
2024-12-17 15:30:44 -0300

    :sparkles: feat: previous state

--- src/components/ListRender.jsx
+++ src/components/ListRender.jsx
@@ -2,6 +2,19 @@ import React from "react";
 
 const ListRender = () => {
   const [list] = React.useState(["Luiz", "Maria", "João"]);
+
+  const [users, setUsers] = React.useState([
+    { id: 1, name: "Gabriel", age: 31 },
+    { id: 2, name: "João", age: 28 },
+    { id: 3, name: "Pedro", age: 44 },
+  ]);
+
+  const deleteRandom = () => {
+    const randomNumber = Math.floor(Math.random() * 4);
+    setUsers((prevUsers) => {
+      return prevUsers.filter((user) => randomNumber !== user.id);
+    });
+  };
   return (
     <div>
       <ul>
@@ -9,6 +22,12 @@ const ListRender = () => {
           <li key={index}>{item}</li>
         ))}
       </ul>
+      <ul>
+        {users.map((item, index) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
+      <button onClick={deleteRandom}>Delete Random user</button>
     </div>
   );
 };
