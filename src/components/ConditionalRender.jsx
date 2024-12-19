b6f4eed769c8c34c0200acb91d5f4b36f2d54fa4
Gabriel Bento <gab.silva.bento@gmail.com>
2024-12-19 13:46:34 -0300

    :sparkles: feat: if/else

--- src/components/ConditionalRender.jsx
+++ src/components/ConditionalRender.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 function ConditionalRender() {
   const [x] = React.useState(false);
+  const [name, setName] = React.useState("João");
   return (
     <div>
       <h1>Isso será exibido?</h1>
       {x && <p>Se x for true, sim</p>}
+      {name === "João" ? (
+        <p>Se name for João, sim</p>
+      ) : (
+        <p>O nome não é João</p>
+      )}
+      <button onClick={() => setName("Maria")}>Alterar nome</button>
     </div>
   );
 }
