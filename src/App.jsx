2c4d36e446379c200e551fe7f7b93d5f9aee19d7
Gabriel Bento <gab.silva.bento@gmail.com>
2024-12-20 14:48:53 -0300

    :sparkles: feat: props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import ConditionalRender from "./components/ConditionalRender";
+import ShowUserName from "./components/ShowUserName";
 
 function App() {
+  const name = "Gabriel";
   return (
     <div className="App">
       <ConditionalRender />
+      <ShowUserName name={name} />
     </div>
   );
 }
