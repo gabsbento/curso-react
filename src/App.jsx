166dfccc94c325edf3e223879ec59b0dcdd7f53d
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-02 17:02:27 -0300

    :sparkles: feat: loop de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,27 @@ import CarDetails from "./components/CarDetails";
 import ConditionalRender from "./components/ConditionalRender";
 
 function App() {
+  const cars = [
+    {id: 1, brand: "VW", color: "Amarelo", status: true, km: 0},
+    {id: 2, brand: "Fiat", color: "Branco", status: true, km: 0},
+    {id: 3, brand: "Hyundai", color: "Prata", status: true, km: 0}
+  ]
   return (
     <div className="App">
       <ConditionalRender />
       <CarDetails brand="VW" color="Vermelho" km={12000} status={false}/>
       <CarDetails brand="Ford" color="Vermelho" km={0} status={true}/>
       <CarDetails brand="Fiat" color="Branco" km={0} status={true}/>
+      {/* Loop */}
+      {cars.map((car, index) => (
+        <CarDetails 
+          brand={car.brand} 
+          color={car.color} 
+          km={car.km} 
+          status={car.status} 
+          key={index}
+        />
+      ))}
     </div>
   );
 }
