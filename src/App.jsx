dedc4d4100353ec6e18e96c242c4c85b48fdfa24
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-02 15:58:09 -0300

    :sparkles: feat: destructuring props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import CarDetails from "./components/CarDetails";
 import ConditionalRender from "./components/ConditionalRender";
 
 function App() {
   return (
     <div className="App">
       <ConditionalRender />
+      <CarDetails brand="VW" color="Vermelho" km={12000}/>
     </div>
   );
 }
