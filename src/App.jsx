f8bad4c1c540ab983f0ea1879ce91c7d56541411
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-03 14:56:25 -0300

    :sparkles: feat: state lift

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import CarDetails from "./components/CarDetails";
 import ConditionalRender from "./components/ConditionalRender";
 import Container from "./components/Container";
 import ExecuteFunction from "./components/ExecuteFunction";
 import Fragment from "./components/Fragment";
+import Message from "./components/Message";
+import ChangeMessageState from "./components/ChangeMessageState";
 
 function App() {
-  const cars = [
+  /*const cars = [
     {id: 1, brand: "VW", color: "Amarelo", status: true, km: 0},
     {id: 2, brand: "Fiat", color: "Branco", status: true, km: 0},
     {id: 3, brand: "Hyundai", color: "Prata", status: true, km: 0}
-  ]
+  ]*/
 
   function showMessage(){
     console.log("Evento do componente pai");
+  }
+
+  const [message, setMessage] = useState("");
+
+  const handleMessage = (msg) => {
+    setMessage(msg);
   }
   return (
     <div className="App">
@@ -22,7 +31,7 @@ function App() {
       <CarDetails brand="Ford" color="Vermelho" km={0} status={true}/>
       <CarDetails brand="Fiat" color="Branco" km={0} status={true}/>
       {/* Loop */}
-      {cars.map((car, index) => (
+      {/*cars.map((car, index) => (
         <CarDetails 
           brand={car.brand} 
           color={car.color} 
@@ -30,13 +39,16 @@ function App() {
           status={car.status} 
           key={index}
         />
-      ))}
+      ))*/}
       <Fragment propFragment={"Terceiro titulo"}/>
       {/** Children */}
       <Container>
         <p>Este é o container</p>
       </Container>
       <ExecuteFunction myFunction={showMessage}/>
+      {/** state lift */}
+      <Message msg={message}/>
+      <ChangeMessageState handleMessage={handleMessage}/>
     </div>
   );
 }
