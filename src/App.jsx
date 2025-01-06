0c45ccc5c251a08b0cde3f26080b76df7df27a63
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-06 10:39:54 -0300

    :sparkles: feat: css de componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import OtherContainer from "./components/OtherContainer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Teste CSS</h1>
+      <OtherContainer />
     </div>
   );
 }
---
+++ src/components/OtherContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./OtherContainer.css";
+
+function OtherContainer() {
+  return (
+    <div className="other-container">
+      <p>Outro container</p>
+      <h6>Outro container</h6>
+    </div>
+  );
+}
+
+export default OtherContainer;
