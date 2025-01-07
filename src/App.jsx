93112ab7729a266aa84eee7144f2679630f9d56b
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-07 09:28:07 -0300

    :sparkles: feat: css module

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import "./App.css";
-import OtherContainer from "./components/OtherContainer";
+import Title from "./components/Title";
 
 function App() {
   let n = 1;
   const redTitle = false;
   return (
     <div className="App">
-      <OtherContainer />
       {/* CSS Inline Dinamico */}
       <h2 style={n < 10 ? { color: "red" } : { color: "green" }}> teste</h2>
       {/* Classe dinamica */}
       <h3 className={redTitle ? "red-title" : "green-title"}> titulo</h3>
+      {/* CSS Module */}
+      <Title />
+      <h1>Teste</h1>
     </div>
   );
 }
---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Title.module.css";
+
+function Title() {
+  return (
+    <div>
+      <h1 className={styles.my_title}>Module CSS</h1>
+    </div>
+  );
+}
+
+export default Title;
