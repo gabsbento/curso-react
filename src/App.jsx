109f765670d12e5e31960b78ef4e6be8d8ca749f
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-07 09:04:10 -0300

    :sparkles: feat: css inline dinamico

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import "./App.css";
 import OtherContainer from "./components/OtherContainer";
 
 function App() {
+  let n = 1;
   return (
     <div className="App">
       <OtherContainer />
+      {/* CSS Inline Dinamico */}
+      <h2 style={n < 10 ? { color: "red" } : { color: "green" }}> teste</h2>
     </div>
   );
 }
