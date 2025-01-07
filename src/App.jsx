94431f723cf6f1b7dccf92a349874ba76f686558
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-07 09:16:30 -0300

    :sparkles: feat: classe css dinamica

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import OtherContainer from "./components/OtherContainer";
 
 function App() {
   let n = 1;
+  const redTitle = false;
   return (
     <div className="App">
       <OtherContainer />
       {/* CSS Inline Dinamico */}
       <h2 style={n < 10 ? { color: "red" } : { color: "green" }}> teste</h2>
+      {/* Classe dinamica */}
+      <h3 className={redTitle ? "red-title" : "green-title"}> titulo</h3>
     </div>
   );
 }
