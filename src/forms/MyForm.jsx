909e8819182d70aee1d2e7a4b3979b103527d47a
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-08 13:38:02 -0300

    :sparkles: feat: gerenciamento de inputs

--- src/forms/MyForm.jsx
+++ src/forms/MyForm.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import "./MyForm.css";
 
 function MyForm() {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+
+  const handleName = (e) => setName(e.target.value);
   return (
     <div>
       {/** */}
       <form>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name="name" placeholder="Digite seu nome" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite seu nome"
+            onChange={handleName}
+          />
         </div>
         <label>
           <span>Email</span>
