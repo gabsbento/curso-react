d5c269bf24fa8a597f03fc05eb7862ca89d0bce9
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-08 10:00:40 -0300

    :sparkles: feat: criando form

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import "./App.css";
-import Title from "./components/Title";
+import MyForm from "./forms/MyForm";
 
 function App() {
-  let n = 1;
-  const redTitle = false;
   return (
     <div className="App">
-      {/* CSS Inline Dinamico */}
-      <h2 style={n < 10 ? { color: "red" } : { color: "green" }}> teste</h2>
-      {/* Classe dinamica */}
-      <h3 className={redTitle ? "red-title" : "green-title"}> titulo</h3>
-      {/* CSS Module */}
-      <Title />
-      <h1>Teste</h1>
+      <h2>Forms</h2>
+      <MyForm />
     </div>
   );
 }
