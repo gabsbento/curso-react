36c2d65f0e68b2da7015fded2b09062984742ee4
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-15 11:04:38 -0300

    :sparkles: feat: projeto - secret word

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Secret Word</h2>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/data/word.jsx
@@ -0,0 +1,11 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Janela", "Teto"],
+  fruta: ["Banana", "Maça", "Uva", "Laranja"],
+  corpo: ["Braço", "Perna", "Ombro", "Punho"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  cidade: ["São Paulo", "Rio de Janeiro", "Salvador", "Curitiba"],
+  animal: ["Cachorro", "Gato", "Papagaio", "Preguica"],
+  comida: ["Macarrão", "Pizza", "Hamburguer", "Bolinho"],
+  musica: ["Rock", "Sertanejo", "MPB", "Pop"],
+  filme: ["Star Wars", "Star Trek", "Pulp Fiction", "The Dark Knight"],
+};
