15eac2a6e1ddd478b6a4af85465ce3e08b6c8dfb
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-15 11:23:37 -0300

    :sparkles: feat: secret word - tela inicial

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./StartScreen.css";
+
+function StartScreen() {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>Clique no botão abaixo para comecar a jogar</p>
+      <button>Comecar o jogo</button>
+    </div>
+  );
+}
+
+export default StartScreen;
