511e8630f60254e12fb1ea05080dd78718412d3e
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-16 14:14:52 -0300

    :sparkles: feat: secret word - sorteando palavras

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,30 @@ const stages = [
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState("");
+
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    return { word, category };
+  };
   const startGame = () => {
+    const { word, category } = pickWordAndCategory();
+
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
     setGameStage(stages[1].name);
   };
   const verifyLetter = () => {
