ef0c502655b35b441b13f734850947de5e00f537
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-30 10:11:53 -0300

    :sparkles: feat: json server - resgatando dados da api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 function App() {
+  const [products, setProducts] = useState([]);
+  const url = "http://localhost:3001/products";
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await fetch(url);
+      const data = await response.json();
+      setProducts(data);
+    }
+
+    getProducts();
+  }, []);
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <ul>
+        {products.map((product) => {
+          return <li key={product.id}>{product.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
