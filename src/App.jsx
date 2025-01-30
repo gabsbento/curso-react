81c7588f805e00162fa9deae50aedfa240629cfc
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-30 10:39:38 -0300

    :sparkles: feat: json server - adicionando dados com react

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { useState, useEffect } from "react";
 function App() {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
   const url = "http://localhost:3001/products";
 
   useEffect(() => {
@@ -13,14 +15,54 @@ function App() {
 
     getProducts();
   }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const product = { name, price };
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+  };
+
   return (
     <div className="App">
       <h1>Hello World</h1>
       <ul>
         {products.map((product) => {
-          return <li key={product.id}>{product.name}</li>;
+          return (
+            <li key={product.id}>
+              {product.name} - R$ {product.price}
+            </li>
+          );
         })}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="text"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <button type="submit">Cadastrar</button>
+        </form>
+      </div>
     </div>
   );
 }
